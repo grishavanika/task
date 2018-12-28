fe49149dbe90fafd0cfc8b7a0bbfdeb2631c5bc4
grisha vanika <grish.vanika@gmail.com>
2018-12-28 15:28:54 +0200

    Fix Clang & VS 2017 with CMake integration.
    Apparently, toolset was fixed and now cmake needs to be invoked with -T LLVM
    with installed "LLVM Compiler Toolchain Visual Studio extension".
    See https://llvm.org/builds/

--- tools/generate_project.py
+++ tools/generate_project.py
@@ -60,7 +60,7 @@ def cmake_win_generator_args(config):
 	args = ['-G', vs_name]
 
 	if config.compiler == Compiler.CLANG:
-		args += ['-T', 'LLVM-vs2014']
+		args += ['-T', 'LLVM']
 
 	if config.vcpkg_path:
 		args += ['-DCMAKE_TOOLCHAIN_FILE={}'.format(config.vcpkg_path)]
