5ff1200631fdb17a1eaae4865299326ffad2a2ca
grisha vanika <grish.vanika@gmail.com>
2018-12-30 11:36:38 +0200

    Specific local environment build_all.bat script that generates & builds all 3 compiles (MSVC, GCC, Clang).
    No-one will ever use this library on another PC :(

---
+++ build_all.bat
@@ -0,0 +1,27 @@
+@echo off
+:: Specific local settings :(
+:: Python, GCC, CMake
+set path=C:\Programs\Python\Python36-64\;%path%;
+set path=%path%;C:\Programs\mingw_gcc_7.3.0\bin;
+set path=%path%;C:\Programs\CMake\bin
+
+:: MSVC
+cd tools
+python generate_project.py --compiler=cl --platform=x64
+cd ../build_cl_x64
+cmake --build . --config Release
+cd ..
+
+:: Clang
+cd tools
+python generate_project.py --compiler=clang --platform=x64
+cd ../build_clang_x64
+cmake --build . --config Release
+cd ..
+
+:: GCC
+cd tools
+python generate_project.py --compiler=gcc --platform=x64
+cd ../build_gcc_x64
+cmake --build . --config Release
+cd ..
