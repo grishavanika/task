f074c9d9694636088bc31d6b586765b61f051b94
grisha vanika <grish.vanika@gmail.com>
2019-02-01 17:51:06 +0200

    Fix Vcpkg CURL integration on Windows.

--- third_party/curl/curl_integration.cmake
+++ third_party/curl/curl_integration.cmake
@@ -5,6 +5,23 @@ if (NOT gcc_on_msvc)
 	# AppVeyor has CURL installed, but it's not for MinGW, skip MinGW now
 	find_package(CURL)
 	if (CURL_FOUND)
+
+		# See Vcpkg issue: https://github.com/Microsoft/vcpkg/issues/1909
+		if (WIN32)
+			list(LENGTH CURL_LIBRARY CURL_LIBRARY_LENGTH)
+			if (CURL_LIBRARY_LENGTH EQUAL 1)
+				set(CURL_LIBRARY_DEBUG_LIB	 ${CURL_LIBRARY})
+				set(CURL_LIBRARY_RELEASE_LIB ${CURL_LIBRARY_DEBUG_LIB}/../../../lib/libcurl.lib)
+				get_filename_component(CURL_LIBRARY_RELEASE_LIB ${CURL_LIBRARY_RELEASE_LIB} REALPATH)
+				unset(CURL_LIBRARY CACHE)
+				unset(CURL_LIBRARY)
+				unset(CURL_LIBRARIES CACHE)
+				unset(CURL_LIBRARIES)
+				set(CURL_LIBRARY "debug;${CURL_LIBRARY_DEBUG_LIB};optimized;${CURL_LIBRARY_RELEASE_LIB}")
+				set(CURL_LIBRARIES ${CURL_LIBRARY})
+			endif ()
+		endif ()
+
 		cmake_print_variables(CURL_LIBRARIES CURL_INCLUDE_DIRS)
 
 		add_library(Curl_Integrated INTERFACE)
